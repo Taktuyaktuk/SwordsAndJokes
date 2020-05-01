8c471e8a61c1996e66a68129416a92c2e1e5e6c1
Adkidus <adrian648@gmail.com>
2020-05-01 21:12:08 +0200

    chodzenie

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float WalkingSpeed = 2f;
+
+    [SerializeField]
+    Animator animator;
+
+    Rigidbody2D Rigidbody;
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        //GetComponent<Entity>().OnKilled += () => Application.Quit();
+    }
+
+    void Update()
+    {
+        this.UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        var WalkingDirection = Vector3.zero;
+
+        WalkingDirection += Vector3.up * Input.GetAxis("Vertical");
+        WalkingDirection += Vector3.right * Input.GetAxis("Horizontal");
+
+        WalkingDirection = WalkingDirection.normalized;
+
+        WalkingDirection *= WalkingSpeed;
+
+        Rigidbody.velocity = WalkingDirection;
+
+        this.Animator();
+    }
+
+    void Animator()
+    {
+        Vector2 move;
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", move.x);
+        animator.SetFloat("Vertical", move.y);
+        animator.SetFloat("Speed", move.sqrMagnitude);
+    }
+}
