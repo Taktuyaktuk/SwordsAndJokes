eae476d50e36977410db1fc791bcde86321e96f1
Adkidus <adrian648@gmail.com>
2020-05-01 20:21:42 +0200

    zmiany 1

---
+++ Assets/Scripts/GenerateLoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab;
+
+    [SerializeField]
+    int min = 2;
+
+    [SerializeField]
+    int max = 15;
+
+    [SerializeField]
+    float probability = 0.25f;
+
+    public void Generate()
+    {
+        if (Random.value > probability)
+            return;
+
+        var loot = Instantiate(prefab);
+        loot.transform.position = transform.position;
+        //loot.GetComponent<Bullets>().Amount = Random.Range(min, max);
+    }
+}
