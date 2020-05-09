3b902b3b6039dbbd1e9cc844ced8c66f92c5f2ff
KaczkaPain <tomaszolszowka1@gmail.com>
2020-05-09 13:55:53 +0200

    Poprawki

--- Assets/Scripts/VolumeValueChange.cs
+++ Assets/Scripts/VolumeValueChange.cs
@@ -8,7 +8,7 @@ public class VolumeValueChange : MonoBehaviour
 
     // Music volume variable that will be modified
     // by dragging slider knob
-    private float musicVolume = 1f;
+    private float musicVolume = 0.05f;
 
     // Use this for initialization
     void Start()
