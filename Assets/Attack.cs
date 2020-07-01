d7e9044a212b14f83e2a7a5744bf9b3fac6a63c2
KaczkaPain <tomaszolszowka1@gmail.com>
2020-07-01 02:00:30 +0200

    Mapa / Uderzanie / Poprawki

---
+++ Assets/Attack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator anim;
+    public KeyCode attack;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(attack))
+
+            anim.SetTrigger("PlayerAttack");
+    }
+}
