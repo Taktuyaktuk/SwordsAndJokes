0dcb811af57c812b393ec817d003a98012dfa7de
Adkidus <adrian648@gmail.com>
2020-07-04 10:14:29 +0200

    hp - canvas

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour
+{
+    [SerializeField]
+    Text HealthCounter;
+
+    void Awake()
+    {
+        FindObjectOfType<Player>().GetComponent<Entity>().OnHealthChanged += health =>
+        {
+            HealthCounter.text = health.ToString("N0");
+        };
+    }
+}
