3a4a6bf783d8a8bd268ffd0ca29c48370764cfea
Adkidus <adrian648@gmail.com>
2020-07-15 22:22:35 +0200

    wciśnij e aby rozmawiać

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    GameObject text;
+
+    private void Awake()
+    {
+        text = GameObject.Find("TextPRESS");
+        text.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            if (text != null)
+                text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            if(text != null)
+                text.SetActive(false);
+        }
+    }
+}
