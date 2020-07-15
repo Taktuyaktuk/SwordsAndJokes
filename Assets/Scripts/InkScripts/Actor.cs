8564f74d5aee73a21a5fd021ebf9eb78532af9be
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-07-15 23:43:23 +0200

    scena z ink jeszcze do przerobienia

---
+++ Assets/Scripts/InkScripts/Actor.cs
@@ -0,0 +1,66 @@
+﻿using Boo.Lang.Environments;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    public Sprite[] emotionSprites;
+    SpriteRenderer spRend;
+    public int ID;
+
+    public enum CharacterEmotion
+    {
+        happy,sad,neutral,angry
+    }
+
+    [SerializeField]
+    CharacterEmotion myState;
+    void Awake()
+    {
+        myState = CharacterEmotion.neutral;
+        spRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeState(string emotionName)
+    {
+        StartCoroutine(emotionName + "State");
+    }
+
+    IEnumerator HappyState()
+    {
+        spRend.sprite = emotionSprites[0];
+        myState = CharacterEmotion.happy;
+        yield return null;
+
+    }
+
+    IEnumerator AngryState()
+    {
+        spRend.sprite = emotionSprites[1];
+        myState = CharacterEmotion.angry;
+        yield return null;
+
+    }
+
+    IEnumerator NeutralState()
+    {
+        spRend.sprite = emotionSprites[2];
+        myState = CharacterEmotion.neutral;
+        yield return null;
+
+    }
+    IEnumerator SadState()
+    {
+        spRend.sprite = emotionSprites[3];
+        myState = CharacterEmotion.sad;
+        yield return null;
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
