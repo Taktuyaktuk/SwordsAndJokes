613caa5d38675f24d31338f8a4dfb41e9c3f2872
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-07-16 17:30:20 +0200

    dodawanie zycia przez poty,tylko dorucic jedna linijke

---
+++ Assets/Scripts/PotionUse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PotionUse : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+   
+
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    public void Use()
+    {
+        //Instantiate(effect, player.position, Quaternion.identity);
+        Destroy(gameObject);
+          
+    }
+}
+
