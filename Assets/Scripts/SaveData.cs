5fa8866b075c844bb50dffde03a31348c172829a
Adkidus <adrian648@gmail.com>
2020-07-17 20:54:58 +0200

    save position

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class SceneSave
+{
+    public string sceneName;
+    public Vector3 position;
+    public Quaternion rotation;
+}
+
+[Serializable]
 public class SaveData
 {
     public string name;
     public float hp;
     public int level;
     public int exp;
-
-    public Vector3 position;
-    public Quaternion rotation;
+    public List<SceneSave> postions;
 }
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveSystem : MonoBehaviour
 {
     static readonly string SAVE_FILE = "player.json";
     string fileName;
+    SaveData saveData;
+
+    float nextSave = 5;
+    float countToSave = 0;
 
     private void Start()
     {
         fileName = Path.Combine(Application.persistentDataPath, SAVE_FILE);
-	Debug.Log(fileName);
         var player = FindObjectOfType<Player>();
         var entity = player.GetComponent<Entity>();
         var lvl = player.GetComponent<LevelSystem>();
@@ -19,12 +24,18 @@ public class SaveSystem : MonoBehaviour
         if (File.Exists(fileName))
         {
             string jsonFromFile = File.ReadAllText(fileName);
-            SaveData saveData = JsonUtility.FromJson<SaveData>(jsonFromFile);
+            saveData = JsonUtility.FromJson<SaveData>(jsonFromFile);
             entity.Health = saveData.hp;
             lvl.Level = saveData.level;
             lvl.Experience = saveData.exp;
-            player.transform.position = saveData.position;
-            player.transform.rotation = saveData.rotation;
+            var m_Scene = SceneManager.GetActiveScene();
+            string sceneName = m_Scene.name;
+            var obj = saveData.postions.Find(x => x.sceneName == sceneName);
+            if (obj != null)
+            {
+                player.transform.position = obj.position;
+                player.transform.rotation = obj.rotation;
+            }
         }
         else
         {
@@ -37,24 +48,52 @@ public class SaveSystem : MonoBehaviour
 
     private void Save()
     {
+        countToSave = 0f;
         var player = FindObjectOfType<Player>();
         var entity = player.GetComponent<Entity>();
         var lvl = player.GetComponent<LevelSystem>();
-        SaveData saveData = new SaveData()
+        var m_Scene = SceneManager.GetActiveScene();
+        string sceneName = m_Scene.name;
+
+        if (saveData == null)
         {
-            name = "player",
-            hp = entity.Health,
-            level = lvl.Level,
-            exp = lvl.Experience,
-            position = player.transform.position,
-            rotation = player.transform.rotation
-        };
-        string JSON = JsonUtility.ToJson(saveData);
+            List<SceneSave> items = new List<SceneSave>()
+            {
+                new SceneSave{ sceneName = sceneName, position = player.transform.position, rotation = player.transform.rotation }
+            };
+            saveData = new SaveData()
+            {
+                name = "player",
+                hp = entity.Health,
+                level = lvl.Level,
+                exp = lvl.Experience,
+                postions = items
+            };
+        }
+        else
+        {
+            saveData.hp = entity.Health;
+            saveData.level = lvl.Level;
+            saveData.exp = lvl.Experience;
+        }
+        var obj = saveData.postions.Find(x => x.sceneName == sceneName);
+        if (obj != null)
+        {
+            obj.position = player.transform.position;
+            obj.rotation = player.transform.rotation;
+        }
+        else
+            saveData.postions.Add(new SceneSave { sceneName = sceneName, position = player.transform.position, rotation = player.transform.rotation });
+
+        string JSON = JsonUtility.ToJson(saveData, true);
         File.WriteAllText(fileName, JSON);
     }
 
     private void Update()
     {
-        Save();
+        if (nextSave > countToSave)
+            countToSave += 1f;
+        else
+            Save();
     }
 }
