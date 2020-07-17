de7c2811712afa71c14f90d4ea902a774073b11f
Adkidus <adrian648@gmail.com>
2020-07-17 22:22:40 +0200

    łuk - myszka

---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    Crosshair crosshair;
+
+    private void Awake()
+    {
+        crosshair = FindObjectOfType<Crosshair>();
+    }
+
+    void Update()
+    {
+        UpdateRotation();
+    }
+
+    void UpdateRotation()
+    {
+        var delta = crosshair.transform.position - transform.position;
+        var targetRotation = (Vector2)delta;
+        transform.right = Vector3.Lerp(transform.right, targetRotation, Time.deltaTime * 10f);
+    }
+}
