3b975564fb68f4f83d78a91df646d5f0e9aafd23
Adkidus <adrian648@gmail.com>
2020-07-17 21:56:42 +0200

    strzelanie

---
+++ Assets/Scripts/BulletDemage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BulletDemage : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var entity = collision.gameObject.GetComponent<Entity>();
+
+        if (entity != null)
+            entity.Health -= 1f;
+
+        Destroy(gameObject);
+    }
+}
