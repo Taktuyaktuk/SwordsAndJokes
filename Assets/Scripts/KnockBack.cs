4e29dbd698ce1d65aee092e5c3b539154531c3f6
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-10-12 21:29:53 +0200

    piwinica 2,proby i bledy,push itd

---
+++ Assets/Scripts/KnockBack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+
+    public Vector2 KnocBackDirection;
+    public float KnockBackPower;
+
+    public SpriteRenderer spriteRenderer;
+    public void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CommitKnockBack(collision.otherRigidbody);
+    }
+
+    private void CommitKnockBack(Rigidbody2D otherRigidbody)
+    {
+        if (Input.GetKey("f"))
+        {
+            otherRigidbody.AddForce(KnocBackDirection * KnockBackPower);
+        }
+    }
+}
+    
