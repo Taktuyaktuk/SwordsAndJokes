f2636f427d4fa1882d3727a97bc3eeeeb91d3960
Adkidus <adrian648@gmail.com>
2020-11-05 18:05:15 +0100

    SLIDER HP

---
+++ Assets/Scripts/SliderHP.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHP : MonoBehaviour
+{
+    [SerializeField]
+    Slider mSlider;
+    private float hp;
+    private float lvl;
+    private void Start()
+    {
+        FindObjectOfType<Player>().GetComponent<Entity>().OnHealthChanged += health =>
+        {
+            hp = health;
+            setSliderValue();
+        };
+        FindObjectOfType<Player>().GetComponent<LevelSystem>().OnLevelUp += Level =>
+        {
+            lvl = Level;
+            setSliderValue();
+        };
+    }
+    private void setSliderValue()
+    {
+        //Debug.Log(mSlider);
+        mSlider.maxValue = lvl*5f;
+        mSlider.value = hp*1f;
+    }
+}
