919340618eb05801e4c6e9f093ebe6354dd82922
Adkidus <adrian648@gmail.com>
2020-11-08 20:33:42 +0100

    magia

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -9,10 +9,7 @@ public class PlayerShooting : MonoBehaviour
     GameObject BulletPref;
 
     [SerializeField]
-    GameObject SpellPref;
-
-    [SerializeField]
-    Boolean NoLimitShots = false;
+    GameObject[] SpellPref;
 
     [SerializeField]
     float BulletSpeed = 2f;
@@ -49,17 +46,44 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(0) && Input.GetKey("space"))
-            Shoot();
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(SpellPref.Length > 0)
+            {
+                Boolean isSpell = false;
+                for (int i = 1; i < 10; i++)
+                {
+                    if (i > SpellPref.Length)
+                        return;
+                    if (Input.GetKey(i.ToString()))
+                    {
+                        isSpell = true;
+                        MagicShot(i);
+                    }
+                }
+                if (!isSpell)
+                    Shoot();
+            } else
+                Shoot();
+        }
     }
 
     void Shoot()
     {
-        if (Bullets < 1 && !NoLimitShots) return;
-        else Bullets--;
+        if (Bullets < 1) return;
+        Bullets--;
 
-        var pref = NoLimitShots ? SpellPref : BulletPref;
-        var bullet = Instantiate(pref);
+        var bullet = Instantiate(BulletPref);
+        bullet.transform.position = transform.position + transform.rotation * (Vector3)ShootPoint;
+        bullet.transform.rotation = bow.transform.rotation;
+
+        var bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = bow.transform.right * BulletSpeed;
+    }
+
+    void MagicShot(int index)
+    {
+        var bullet = Instantiate(SpellPref[index - 1]);
         bullet.transform.position = transform.position + transform.rotation * (Vector3)ShootPoint;
         bullet.transform.rotation = bow.transform.rotation;
 
