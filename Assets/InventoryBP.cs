5539f1875b2f06c3e69b896f2a322fd0b2c56052
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-11-13 20:34:21 +0100

    invetroy00.1

---
+++ Assets/InventoryBP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryBP : MonoBehaviour
+{
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject slotHolder;
+
+    private void Start()
+    {
+        allSlots = 42;
+        slot = new GameObject[allSlots];
+
+        for (int i =0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryEnabled = !inventoryEnabled;
+
+        if(inventoryEnabled ==true)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
