b05e27ff0caba10d49cfbd3fa1c5e4496ebfe574
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-11-24 17:18:24 +0100

    Strzala juz nie trafia w gracz strzelajac w dól
    ale trzeba to przetestowac ale wygalda na to ze ok

--- Assets/Scripts/BulletDemage.cs
+++ Assets/Scripts/BulletDemage.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class BulletDemage : MonoBehaviour
 {
+    Rigidbody2D m_rigidbody2d;
+
+    void Start()
+    {
+        m_rigidbody2d = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreLayerCollision(15, 0);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.gameObject.GetComponent<Player>();
