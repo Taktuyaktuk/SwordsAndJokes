2a786086e54ada26f496f8b227d328dd5869b9d6
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2020-12-17 21:18:28 +0100

    nareszcie obszaar czaru dziala tak jak powinnien

--- Assets/Prefabs/Projectiles/FireballColldmg.cs
+++ Assets/Prefabs/Projectiles/FireballColldmg.cs
@@ -8,16 +8,28 @@ public class FireballColldmg : MonoBehaviour
     Mob TargetMob;
     [SerializeField]
     float AttackDemage = 2f;
-    
-    
+    Rigidbody2D m_rigidbody2d;
+
+
+    void Start()
+    {
+        m_rigidbody2d = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreLayerCollision(0, 8);
+        
+    }
 
     public void OnTriggerEnter2D(Collider2D collision) //jarek do inveotry 17.11.20
     {
-
+        
         var mob = collision.gameObject.GetComponent<Mob>();
-       
+        
+
+
+
         if (mob != null)
         {
+
+            
             TargetMob = mob;
             Debug.Log(TargetMob);
             TargetMob.GetComponent<Entity>().Health -= AttackDemage;
