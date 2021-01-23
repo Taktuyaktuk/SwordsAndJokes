8a3d6c8eab6ffd6ea0d6c90a6b1f7342de321a8e
Adkidus <adrian648@gmail.com>
2021-01-23 20:27:33 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/mglifesgood96/RPG-Game""
    This reverts commit 08a6549b86b1a8964da6db663821165591b7d623.

---
+++ Assets/Quest/QuestDialog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestDialog : MonoBehaviour
+{
+    public void Close()
+    {
+        gameObject.active = false;
+    }
+
+    public void Open()
+    {
+        Debug.Log("OPEN");
+    }
+}
