6d3123d6b821b6b2a3637dd08caa75c5919c0b03
Taktuyaktuk <44909238+Taktuyaktuk@users.noreply.github.com>
2021-02-19 22:52:47 +0100

    instan potion

---
+++ Assets/PotionInstantUse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionInstantUse : MonoBehaviour
+{
+    Player targetPlayer;
+    [SerializeField]
+    float HP;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+        {
+            targetPlayer = player;
+            targetPlayer.GetComponent<Entity>().Health += HP;
+            Destroy(this.gameObject);
+        }
+    }
+}
