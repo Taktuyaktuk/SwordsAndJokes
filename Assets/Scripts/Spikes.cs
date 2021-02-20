98b1edd51dd8b51d365c2824f74b1ef3e7a24584
Adkidus <adrian648@gmail.com>
2021-02-20 13:57:42 +0100

    spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+
+    [SerializeField]
+    public float AttackCooldown = 2f;
+
+    [SerializeField]
+    public float AttackDamage = 1f;
+
+    public float AttackTime = 0;
+
+    private Entity entity;
+
+    void Update()
+    {
+        if(entity != null)
+        {
+            AttackTime += Time.deltaTime;
+            if (AttackTime >= AttackCooldown)
+            {
+                entity.Health -= AttackDamage * Time.deltaTime;
+                AttackTime = 0;
+            }
+        }
+        else
+        {
+            AttackTime = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var ent = collision.gameObject.GetComponent<Entity>();
+        if (ent != null)
+            entity = ent;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var ent = collision.gameObject.GetComponent<Entity>();
+        if (ent != null)
+            entity = null;
+    }
+}
